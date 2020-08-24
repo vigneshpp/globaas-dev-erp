b1db5696d9d2fda191a755c1ec1f5ee2b8de61f5
vigneshpp <vigneshpp@users.noreply.github.com>
2020-08-25 00:32:33 +1200

    loader splash added

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -80,7 +80,7 @@ website_generators = ["Item Group", "Item", "BOM", "Sales Partner",
 
 website_context = {
 	"favicon": 	"/assets/erpnext/images/favicon.png",
-	"splash_image": "/assets/erpnext/images/erp-icon.svg"
+	"splash_image": "/assets/erpnext/images/wedges-splash.gif"
 }
 
 website_route_rules = [
